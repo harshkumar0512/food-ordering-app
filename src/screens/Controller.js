147fb61dbf1f2d83b1684f1f0d327ccf98806a65
harshkumar0512 <55206284+harshkumar0512@users.noreply.github.com>
2021-05-03 07:29:59 +0530

    added order related files from mates

--- src/screens/Controller.js
+++ src/screens/Controller.js
@@ -2,6 +2,8 @@ import React, {Component} from 'react';
 import Home from '../screens/home/Home';
 import Profile from "../screens/profile/Profile"
 import {BrowserRouter as Router, Route} from 'react-router-dom';
+import Checkout from "./checkout/Checkout";
+import Details from "./details/Details";
 
 class Controller extends Component {
 
@@ -16,6 +18,9 @@ class Controller extends Component {
                 <div className="main-container">
                     <Route exact path='/' render={(props) => <Home {...props} baseUrl={this.baseUrl}/>}/>
                     <Route exact path='/profile' render={(props) => <Profile {...props} />}/>
+                    <Route exact path="/checkout" render={(props) => <Checkout {...props} baseUrl={this.baseUrl} />} />
+                    <Route exact path='/restaurant/:restaurantId'
+                           render={(props) => <Details {...props} baseUrl={this.baseUrl} />} />
                 </div>
             </Router>
         )
