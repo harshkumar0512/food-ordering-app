25858718332087ff2d4d6f6f976a7af16c5873f4
Jitesh Acharya <jitesh.acharya2400@gmail.com>
2021-05-03 11:10:16 +0530

    Added checkout functionlaity and snackbar

--- src/screens/details/Details.js
+++ src/screens/details/Details.js
@@ -7,7 +7,6 @@ import { faStar, faCircle, faRupeeSign } from '@fortawesome/free-solid-svg-icons
 import { faStopCircle } from '@fortawesome/free-regular-svg-icons';
 import Divider from '@material-ui/core/Divider';
 import IconButton from '@material-ui/core/IconButton';
-import { withStyles } from '@material-ui/core/styles';
 import Add from '@material-ui/icons/Add';
 import Remove from '@material-ui/icons/Remove';
 import Snackbar from '@material-ui/core/Snackbar';
@@ -17,20 +16,10 @@ import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
 import Card from '@material-ui/core/Card';
 import Badge from '@material-ui/core/Badge';
 import Button from '@material-ui/core/Button';
+import { Link } from 'react-router-dom';
+import Typography from '@material-ui/core/Typography';
 
 library.add(faStar, faCircle, faRupeeSign, faStopCircle)
-
-const styles = theme => ({
-    button: {
-        margin: theme.spacing.unit,
-    },
-    icon: {
-        margin: theme.spacing.unit,
-    },
-    badge: {
-        margin: theme.spacing.unit * 2,
-    }
-});
 
 class Details extends Component {
 
@@ -40,11 +29,11 @@ class Details extends Component {
             restaurantDetail: {},
             address: {},
             categories: [],
-            snackBarOpen: false,
-            snackBarMessage: "",
-            cartCounter: 0,
+            cartCount: 0,
             cartItems: [],
-            totalCartValue: 0
+            totalCartPrice: 0,
+            snackBarOpen: false,
+            snackBarMessage: ""
         }
     }
 
@@ -56,7 +45,6 @@ class Details extends Component {
         xhrRestaurants.addEventListener("readystatechange", function () {
             if (this.readyState === 4) {
                 let response = JSON.parse(this.responseText);
-                console.log(response);
                 that.setState({
                     restaurantDetail: response,
                     address: response.address,
@@ -64,15 +52,74 @@ class Details extends Component {
                 });
             }
         });
-
-        xhrRestaurants.open('GET', this.props.baseUrl + 'restaurant/2461973c-a238-11e8-9077-720006ceb890');
+        xhrRestaurants.open('GET', this.props.baseUrl + 'restaurant/' + this.props.match.params.restaurantId);
         xhrRestaurants.setRequestHeader("Content-Type", "application/json");
         xhrRestaurants.setRequestHeader("Cache-Control", "no-cache");
         xhrRestaurants.send(data);
     }
 
-    addItemClickHandler(item) {
-        console.log(item);
+    addItemClickHandler(item, isAddedFromCart) {
+        const itemIndex = this.state.cartItems.findIndex(cartItem => cartItem.id === item.id);
+        var updatedCartItems = this.state.cartItems;
+
+        if (itemIndex === -1) {
+            item.quantity = 1;
+            item.cartPrice = item.price;
+            updatedCartItems.push(item);
+        } else {
+            updatedCartItems[itemIndex].quantity++;
+            updatedCartItems[itemIndex].cartPrice = item.price * updatedCartItems[itemIndex].quantity;
+        }
+
+        this.setState({ cartItems: updatedCartItems });
+        this.setState({ cartCount: this.state.cartCount + 1 });
+        this.setState({ totalCartPrice: this.state.totalCartPrice + item.price });
+        this.setState({ snackBarOpen: true });
+        this.setState({ snackBarMessage: isAddedFromCart ? "Item quantity increased by 1!" : "Item added to cart!" });
+    }
+
+    removeItemClickHandler(item) {
+        const itemIndex = this.state.cartItems.findIndex(cartItem => cartItem.id === item.id);
+        var updatedCartItems = this.state.cartItems;
+
+        if (item.quantity === 1) {
+            updatedCartItems.splice(itemIndex, 1);
+            this.setState({ snackBarOpen: true });
+            this.setState({ snackBarMessage: "Item removed from cart!" });
+        } else {
+            updatedCartItems[itemIndex].quantity--;
+            updatedCartItems[itemIndex].cartPrice = item.price * updatedCartItems[itemIndex].quantity;
+            this.setState({ snackBarOpen: true });
+            this.setState({ snackBarMessage: "Item quantity decreased by 1!" });
+        }
+
+        this.setState({ cartItems: updatedCartItems });
+        this.setState({ cartCount: this.state.cartCount - 1 });
+        this.setState({ totalCartPrice: this.state.totalCartPrice - item.price });
+    }
+
+    checkoutClickHandler = () => {
+        if (this.state.cartItems.length === 0 && this.state.cartCount === 0) {
+            this.setState({ snackBarOpen: true });
+            this.setState({ snackBarMessage: "Please add an item to your cart!" });
+            return;
+        }
+        if (sessionStorage.getItem("access-token") === null) {
+            this.setState({ snackBarOpen: true });
+            this.setState({ snackBarMessage: "Please login first!" });
+            return;
+        }
+        /* Need to check navigation part */
+        this.props.history.push({
+            pathname: 'checkout',
+            restaurantName: this.state.restaurantDetail.restaurant_name,
+            cartItems: this.state.cartItems,
+            totalCartPrice: this.state.totalCartPrice
+        });
+    }
+
+    closeSnackbar = () => {
+        this.setState({ snackBarOpen: false });
     }
 
     render() {
@@ -81,11 +128,18 @@ class Details extends Component {
         const address = this.state.address;
         const categories = this.state.categories;
         const cartItems = this.state.cartItems;
-        const totalCartValue = this.state.totalCartValue;
-        const { classes } = this.props;
+        const totalCartPrice = this.state.totalCartPrice;
+
         return (
             <div className='details-container'>
-                <Header {...this.props} isHomePage={false} />
+                <Header {...this.props} showSearch={false} />
+
+                <Link to={"/"}>
+                    <Typography className='back' >
+                        &#60; Back
+                </Typography>
+                </Link>
+
                 <div className='restaurant-detail-container'>
                     <div className='restaurant-image-container'>
                         <img className='restaurant-image' src={restaurantDetail.photo_URL} />
@@ -123,20 +177,20 @@ class Details extends Component {
                                 <p style={{ textTransform: 'uppercase' }}>{category.category_name}</p>
                                 <Divider className="category-divider" />
                                 {category.item_list.map((item) => (
-                                    <table key={item.id}>
+                                    <table key={item.id} width='100%'>
                                         <tbody>
                                             <tr>
                                                 <td width='10%' className={item.item_type}>
                                                     <FontAwesomeIcon icon="circle" />
                                                 </td>
-                                                <td width="65%" className="item-name">
+                                                <td width="50%" className="item-name">
                                                     {item.item_name}
                                                 </td>
-                                                <td width="20%">
+                                                <td width="30%">
                                                     <FontAwesomeIcon icon="rupee-sign" /> {(item.price).toFixed(2)}
                                                 </td>
                                                 <td>
-                                                    <IconButton onClick={() => this.addItemClickHandler(item)} >
+                                                    <IconButton onClick={() => this.addItemClickHandler(item, false)} >
                                                         <Add />
                                                     </IconButton>
                                                 </td>
@@ -148,12 +202,89 @@ class Details extends Component {
                         ))}
                     </div>
                     <div className='cart-container'>
-                        Hello he
+                        <Card style={{ padding: '5%' }}>
+                            <div style={{ marginTop: '2%' }}>
+                                <Badge badgeContent={this.state.cartCount > 0 ? this.state.cartCount : '0'}
+                                    color="primary">
+                                    <ShoppingCartIcon />
+                                </Badge>
+                                <span className='cart-title'>My Cart</span>
+                            </div>
+                            <br /><br />
+                            {cartItems.map((cartItem) => (
+                                <table key={cartItem.id}>
+                                    <tbody>
+                                        <tr height='10px'>
+                                            <td width="10%" className={cartItem.item_type}>
+                                                <FontAwesomeIcon className={cartItem.item_type} icon={["far", "stop-circle"]} />
+                                            </td>
+                                            <td width="30%" className="item-name">
+                                                {cartItem.item_name}
+                                            </td>
+                                            <td width="5%">
+                                                <IconButton onClick={() => this.removeItemClickHandler(cartItem)}>
+                                                    <Remove />
+                                                </IconButton>
+                                            </td>
+                                            <td width="5%" style={{ textAlign: 'center' }}>
+                                                {cartItem.quantity}
+                                            </td>
+                                            <td width="5%">
+                                                <IconButton onClick={() => this.addItemClickHandler(cartItem, true)}>
+                                                    <Add />
+                                                </IconButton>
+                                            </td>
+                                            <td width="40%" style={{ color: 'gray', textAlign: 'right' }}>
+                                                <FontAwesomeIcon icon="rupee-sign" /> {(cartItem.cartPrice).toFixed(2)}
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            ))}
+                            <br /><br />
+                            <table width='100%'>
+                                <tbody>
+                                    <tr>
+                                        <td width='50%' className='bold'>TOTAL AMOUNT</td>
+                                        <td className='total-amount'><FontAwesomeIcon icon="rupee-sign" /> {(totalCartPrice).toFixed(2)}</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                            <br /><br />
+                            <Button variant="contained" color="primary" style={{ width: '100%' }}
+                                onClick={this.checkoutClickHandler}>
+                                CHECKOUT
+                            </Button>
+                        </Card>
                     </div>
                 </div>
+                <Snackbar
+                    anchorOrigin={{
+                        vertical: 'bottom',
+                        horizontal: 'left',
+                    }}
+                    open={this.state.snackBarOpen}
+                    autoHideDuration={6000}
+                    onClose={this.closeSnackbar}
+                >
+                    <SnackbarContent
+                        onClose={this.closeSnackbar}
+                        message={this.state.snackBarMessage}
+                        action={[
+                            <IconButton
+                                key="close"
+                                aria-label="Close"
+                                color="inherit"
+                                onClick={this.closeSnackbar}
+                            >
+                                <CloseIcon />
+                            </IconButton>,
+                        ]}
+                    />
+                </Snackbar>
             </div>
         )
     }
 }
 
-export default withStyles(styles)(Details);
+export default Details;
